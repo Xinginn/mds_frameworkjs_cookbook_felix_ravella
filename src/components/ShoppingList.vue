2c3e8955e7006a3052d04ec429f8651bac326d9b
felix.ravella <felix.ravella@gmail.com>
2023-03-15 13:25:53 +0100

    feat: add mass check and clear

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -6,6 +6,9 @@
     :ingredient="item"
     @boughtChanged="onItemBoughtChanged"
   />
+  <button type="button" @click="checkAll">Check all</button>
+  <button type="button" @click="clearAllChecked">Clear checked items</button>
+  <button type="button" @click="clearAll">Clear all</button>
 </template>
 
 <script>
@@ -33,6 +36,19 @@ export default {
   methods: {
     onItemBoughtChanged(index, value){
       this.mutableData[index].bought = value;
+      this.$emit('dataChanged', this.mutableData);
+    },
+    checkAll(){
+      this.mutableData.map(item => item.bought = true);
+      this.$emit('dataChanged', this.mutableData);
+    },
+    clearAll(){
+      this.$emit('dataChanged', []);
+    },
+    clearAllChecked(){
+      this.mutableData = this.mutableData.filter((item) => {
+        return item.bought === false
+      })
       this.$emit('dataChanged', this.mutableData);
     }
   }
