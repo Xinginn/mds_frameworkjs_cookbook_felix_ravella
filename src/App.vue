85d4713614457dac75c82fdef4f0bffd63236202
felix.ravella <felix.ravella@gmail.com>
2023-03-15 10:23:25 +0100

    feat: display recipe buttons + handle selection & deletion

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Recipe Book</h1>
+  <RecipeButton v-for="item,index of recipes" 
+    :key="index"
+    :recipeIndex="index"
+    :title="item.title"
+    @requestEdit="(recipeIndex) => selectedRecipe = recipeIndex"
+    @requestDelete="(recipeIndex) => recipes.splice(recipeIndex, 1)"
+    />
 </template>
 
+<script lang="ts">
+import RecipeButton from './components/RecipeButton.vue'
+
+export default {
+  name: 'App',
+  components:{
+    RecipeButton
+  },
+  data(){
+    return {
+      recipes: [
+        {
+          title: "Beef stew",
+          description: "A delicious dish from England made with beef",
+          ingredients: [
+            "1 carrot",
+            "2 potatoes",
+            "200g beef"
+          ]
+        },        
+        {
+          title: "Cabage soup",
+          description: "A delicious dish from England made with beef",
+          ingredients: [
+            "1 cabage",
+            "3 potatoes",
+            "2 onions"
+          ]
+        },
+      ],
+      selectedRecipe: null
+    }
+  }
+  
+}
+</script>
+
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/RecipeButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <span>{{title}}</span>
+  <button type="button" @click="() => $emit('requestEdit', recipeIndex)">Edit</button>
+  <button type="button" @click="() => $emit('requestDelete', recipeIndex)">Delete</button>
+</template>
+
+<script lang="ts">
+export default {
+  name:'RecipeButton',
+  props: {
+    recipeIndex: Number,
+    title: String
+  },
+  emits:[
+    'requestEdit',
+    'requestDelete',
+  ]
+}
+</script>
